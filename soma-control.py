391f7b867cd15b1aa50e7943104499f64908557e
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2023-03-25 13:59:53 -0400

    Update soma-control.py

--- soma-control.py
+++ soma-control.py
@@ -35,13 +35,30 @@ def open_shade(host,mac):
 	devices = requests.get(url)
 # Function for closing the shades 
 def close_shade(host,mac): 
-
 	# If you have a U1 change to match its API 
 	# https://support.somasmarthome.com/hc/en-us/articles/360026064234-HTTP-API
 
 	url = f"http://{host}:3000/close_shade/{mac}"
 	# Close the device one at a time 
 	devices = requests.get(url)
+# Function for listing the battery levels of the shades 
+def list_battery(host, mac): 
+	url = f"http://{host}:3000/get_battery_level/{mac}"
+	battery_level = requests.get(url)
+	return battery_level.json()['battery_percentage']
+# Function for listing the state of the shades 
+def shade_state(host,mac): 
+	#get_shade_state
+	url = f"http://{host}:3000/get_shade_state/{mac}"
+	shade_state = requests.get(url)
+	return shade_state.json()['position']
+# Function for light levels of the shades 
+def light_level(host,mac): 
+	#get_light_level
+	url = f"http://{host}:3000/get_light_level/{mac}"
+	shade_state = requests.get(url)
+	return shade_state.json()['light_level']
+
 #
 # MAIN function 
 #
@@ -51,7 +68,9 @@ def main():
 	
 	# Set up arguments
 	parser.add_argument('host', type=str, help="The host ot connect to e.g.: 127.0.01")
-	parser.add_argument('action',type=str, help="The action you want to perform open/close")
+	parser.add_argument('action',type=str, help="The action you want to perform: open, close, \
+		battery, state, light")
+
 	# Parse the arguments 
 	args = parser.parse_args()
 	# set the host to the passed in host and the action
@@ -60,13 +79,29 @@ def main():
 	# store the names and mac address for each device 
 	macs, names = list_devices(host)
 	
+	x = 0
 	#for each device in the macs list, open or close it based on the action 
 	for device in macs: 
 		if action == "open": 
 			open_shade(host, device)
 		elif action == "close":
 			close_shade(host, device)
-		else: 
+		elif action == "battery":
+			print(f"--- {names[x]} is {list_battery(host, device)}%")
+			x += 1
+		elif action == "state":
+			position = shade_state(host, device)
+			if position == 0: 
+				s_pos = "Open"
+			else: 
+				s_pos = "Closed"
+			print(f"--- {names[x]} is {s_pos}")
+			x += 1 
+		elif action == "light":
+			print(f"--- {names[x]} light level {light_level(host, device)}")
+			x += 1
+
+		else:
 			print(f"Wrong Option {action}")
 
 if __name__ == "__main__":
